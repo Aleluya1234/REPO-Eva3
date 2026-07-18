1361c51a01aacb44a6dca01db3665e257cf03a05
BenjaminN. <benjaminmoreno0809@gmail.com>
2026-07-18 20:44:09 +0000

    arreglo

--- REPO-Eva-3/src/pages/ProductosAPI.jsx
+++ REPO-Eva-3/src/pages/ProductosAPI.jsx
@@ -10,7 +10,7 @@ function ProductosAPI() {
     const obtenerProductos = async () => {
       try {
         setCargando(true);
-        const response = await fetch('/api/productos');
+        const response = await fetch('http://localhost:8000/api/productos');
         
         if (!response.ok) {
           throw new Error(`Error ${response.status}: ${response.statusText}`);
